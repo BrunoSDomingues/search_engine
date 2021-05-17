8500fdbdc78d6c95d8830131d5debf30d63d6116
Bruno Domingues <bruno_signorelli@outlook.com>
2021-05-17 19:27:56 -0300

    Updated index to use class + normalizer

--- se/index.py
+++ se/index.py
@@ -1,22 +1,45 @@
 import json
 
 from collections import defaultdict
+from .normalizer import clean_text
+
+
+class Index:
+    def __init__(self, index):
+        self.index = index
+
+    def find(self, query_term):
+        return self.index[query_term] if query_term in self.index else []
+
+    def countWords(self, query_term, doc_number):
+        try:
+            return (
+                self.index[query_term][doc_number]
+                if doc_number in self.index[query_term]
+                else 0
+            )
+        except KeyError as k:
+            return 0
 
 
 def make_index(docs):
-    index = defaultdict(list)
+    index = defaultdict(lambda: defaultdict(int))
+
     for k, doc in enumerate(docs):
         words = set(doc)
+        words = map(clean_text, words)
+
         for word in words:
-            index[word].append(k)
+            index[word][k] += 1
+
     return index
 
 
 def save_index(index, path):
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         json.dump(index, file, indent=4)
 
 
 def load_index(path):
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         return json.load(file)
