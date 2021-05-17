96d7240752a79320ac33af1f52bb834bede4b59d
Bruno Domingues <bruno_signorelli@outlook.com>
2021-05-17 19:30:30 -0300

    Search now using index and new functions from query

--- se/search.py
+++ se/search.py
@@ -1,10 +1,17 @@
-from se.query import parse_json_query
-from se.retrieve import retrieve
-from se.rank import rank_documents
+from .query import parse_raw_query, parse_json_query
+from .rank import rank_documents
+from .index import Index
 
 
 def search(raw_query, index, docs):
-    query = parse_json_query(raw_query)
-    index_query = query.evaluate(index)
-    ranked_index = rank_documents(query, docs, index_query)
+    parsedquery = parse_json_query(parse_raw_query(raw_query))
+
+    raw_query = raw_query.split()
+    raw_query = [raw_query[i] for i in range(len(raw_query)) if i % 2 == 0]
+
+    idx = Index(index)
+
+    index_query = parsedquery.evaluate(idx)
+    ranked_index = rank_documents(raw_query, docs, index_query, idx)
+
     return [docs[k] for k in ranked_index]
